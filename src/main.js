c573bc4d232dbdcab1405606b4c4947f57e22cb9
Everton Roberto <eoliveira@alelo.com.br>
2020-06-06 12:32:48 -0300

    chore: adicionado font-awersome no projeto

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-Vue.config.productionTip = false
+import { faCopyright } from "@fortawesome/free-regular-svg-icons";
+
+library.add(faCopyright);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
